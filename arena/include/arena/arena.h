9e42631eb2776181448fcacb06fa1da026a60d97
Sebastian Bergt <sebastian@bergt.org>
2021-03-22 15:49:40 +0100

    stl lists are slower than custom list

--- arena/include/arena/arena.h
+++ arena/include/arena/arena.h
@@ -2,8 +2,8 @@
 #ifndef ARENA_INCLUDE_ARENA_ARENA_H
 #define ARENA_INCLUDE_ARENA_ARENA_H
 
-#include <arena/arena_element.h>
 #include <arena/i_arena.h>
+#include <list>
 #include <type_traits>
 #include <vector>
 
@@ -24,55 +24,37 @@ public:
 
   bool initialize(std::size_t size) override {
     memory_ = std::vector<T>(size, T{});
-    list_elements_ =
-        std::vector<detail::ArenaElement<T>>(size, {nullptr, nullptr});
-    if (!initialize_list_pointers(size)) {
-      return false;
-    }
     // all are free
-    free_list_ = &list_elements_[0];
-    in_use_list_ = nullptr;
-    initialized_ = true;
+    for (std::size_t i = 0; i < size; i++) {
+      free_list_.push_back(&memory_[i]);
+    }
 
-    // initialize arena deleter
+    // initialize Arena deleter
     reusage_deleter_ = [this](T *t) {
-      auto arena_element = detail::ArenaElement<T>::move_front(
-          this->in_use_list_, this->free_list_);
-      arena_element->t = t;
+      if (t) {
+        free_list_.splice(free_list_.end(), in_use_list_, in_use_list_.begin());
+        free_list_.back() = t;
+      }
     };
+
+    initialized_ = true;
 
     return true;
   }
 
   arena_ptr<T> create() override {
-    auto arena_element =
-        detail::ArenaElement<T>::move_front(free_list_, in_use_list_);
-    if (!arena_element) {
+    if (free_list_.empty()) {
       return {nullptr, reusage_deleter_};
     }
-    return {arena_element->t, reusage_deleter_};
+    in_use_list_.splice(in_use_list_.end(), free_list_, free_list_.begin());
+    return {in_use_list_.back(), reusage_deleter_};
   }
 
 private:
-  bool initialize_list_pointers(std::size_t size) {
-    // all but last
-    for (std::size_t i = 0; i < size - 1; i++) {
-      list_elements_[i].t = &memory_[i];
-      list_elements_[i].next = &list_elements_[i + 1];
-    }
-    // last
-    list_elements_[size - 1].t = &memory_[size - 1];
-    list_elements_[size - 1].next = nullptr;
-
-    return true;
-  }
-
   bool initialized_{false};
   std::vector<T> memory_; // actual elements are stored here
-  std::vector<detail::ArenaElement<T>>
-      list_elements_; // actual elements are stored here
-  detail::ArenaElement<T> *free_list_;
-  detail::ArenaElement<T> *in_use_list_;
+  std::list<T *> free_list_{};
+  std::list<T *> in_use_list_{};
   std::function<void(T *t)> reusage_deleter_;
 };
 
